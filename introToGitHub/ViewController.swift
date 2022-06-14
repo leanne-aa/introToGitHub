9d8ee3bef6cc14d986b1cc36b7fef40277924d9f
Leanne Afrifa <l.afrifa04@icloud.com>
2022-06-14 17:24:18 +0000

    Initial Commit

---
+++ introToGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introToGitHub
+//
+//  Created by Awura Ama on 6/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
